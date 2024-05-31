7c3bf69fdd6a79336c49158e4da8f103daea17ea
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-01 01:14:15 +0530

    Q11_Fun.py

---
+++ Exercises Fun - Level2/Q11_Fun.py
@@ -0,0 +1,18 @@
+# Declare a function named print_list. It takes a list as a parameter and it prints out
+# each element of the list.
+
+def print_list(my_list):
+    """
+    Print the elements of a list.
+    
+    Parameters:
+    my_list (list): The list to be printed.
+    
+    Return:
+    Nono
+    """
+    for element in my_list:
+      print(element)
+# Example :
+Rao_list = [ 3,-5,6,23,34]
+print_list(Rao_list)   

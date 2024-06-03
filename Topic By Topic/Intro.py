bb872b44e24bc48a8a47198dbd4ee74e70ed7e85
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-04 00:52:49 +0530

    Intro.py

---
+++ Topic By Topic/Intro.py
@@ -0,0 +1,63 @@
+# First promgram in python:
+
+print("Hello Rao hammad")
+
+# Varablies Declearing and Types:
+
+        # 1) Text Type:
+# Sting (str): A sequence of characters enclosed in single or double quotes.
+
+name = "Ali"
+print(name)
+        # 2) Numeric Types:
+
+# 1) Interger(int): Whole numbers without any decimal points.
+
+num_student = 24
+print(num_student)
+
+# 2) Float(float): Numbers with decimal points or in exponential form.
+
+hight = 4.6
+print(hight)
+
+    # 3) Boolean Type:
+# Boolean(bool):Represents either True or False.
+
+is_sunny = True
+print("Is it sunny today?", is_sunny)
+
+    # 4) Sequence Type:
+# 1) List:An ordered, mutable collection of elements.
+
+numbers =  [1,4,3,4,5]
+print(numbers)
+
+# 2) Tuple:An ordered, immutable collection of elements.
+
+coordinates = (3,5)
+print(coordinates)
+
+# 3) Range:
+""" is a built-in Python type representing an immutable
+sequence of numbers, commonly used for generating integer 
+sequences, such as for iterating over a range of values."""
+          
+numbers_sequence = range(5)
+print("Generated sequence:", list(numbers_sequence))
+
+        # 5) Set Type:
+# Set: An unordered collection of unique elements.
+
+num_set = {20,45,67,40,True,'Rao'}
+print(num_set)
+        # 6) Mapping Type:
+# Dictionary (dict): A collection of key-value pairs.
+
+person = {'name','ali',34,37}
+print(person)
+        # 7) None Type:
+# None Type (none):Represents the absence of a value or a null value.
+
+result = None
+print(result)

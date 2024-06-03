3c16dfd3a945393fbe42cfd97eb5be69e30e4b9c
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-04 00:54:15 +0530

    Create Type_Casting.py

---
+++ Topic By Topic/Type_Casting.py
@@ -0,0 +1,26 @@
+# Tyep Casting:In Python, type casting refers to the process of converting a variable 
+# from one data type to another. This is also known as type conversion. 
+# Python provides several built-in functions for type casting, 
+# allowing you to convert variables from one data type to another.
+
+# Explain With Example:1
+
+num_str = "123"
+print(num_str)
+print(type(num_str))
+
+# Converent interger:
+
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+# Example no:2
+
+# This is string value
+str1 = "20"
+str2 = "45"
+# srting value converet interger number
+
+print(int(str1)+int(str2))
+

a6cc25700464e19054b252176a032d17fc4ede76
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-05 01:52:13 +0530

    IndexError.py

---
+++ Topic By Topic/IndexError.py
@@ -0,0 +1,9 @@
+# Index Error:in Python occurs when you try to access an index in a sequence 
+# (like a list or tuple) that is outside the valid range of indices for that sequence.
+# The error typically indicates that
+# the index you're trying to access does not exist in the sequence.
+
+my_list = [1,2,3,5,6,7]
+print(my_list[1])
+
+# print(my_list[10])

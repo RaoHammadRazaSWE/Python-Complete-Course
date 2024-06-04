18ce352c4575a7df192d20020644ca02261be7ab
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-05 01:52:56 +0530

    Input_function.py

---
+++ Topic By Topic/Input_function.py
@@ -0,0 +1,10 @@
+# name = input("what is your name?")
+
+# print("Hello" " " + input("what is your name?") )
+
+print("Hey" + " " + input("what is your name?") + " , " + "How are you?")
+
+# Assing variables values
+
+a = 35
+print(a)

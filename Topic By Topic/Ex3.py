dacdb1a35a772638f04c458915f3aa246fb4e294
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-07 04:59:34 +0900

    Ex3.py

---
+++ Topic By Topic/Ex3.py
@@ -0,0 +1,41 @@
+# Calculates the Body Mass Index (BMI)
+# Weight = input("Enter weight in kg:")
+# Height = input("Enter height is meter:")
+# BMI = int(Weight)/float(Height) ** 2
+# print(int(BMI))
+
+# Vowels and consantant count.
+# def count_vowels_and_consonants(string):
+#   vowels = "aeiouAEIOU"
+#   constants = "bcdgt"
+
+#   vowel_count = 0
+#   consonant_count = 0
+
+#   for char in string:
+#     if char in vowels:
+#       vowel_count +=1
+#     elif char in constants:
+#         consonant_count +=1
+#   return vowel_count, consonant_count
+# input_string = input("Enter a string:")
+# vowels, consonants = count_vowels_and_consonants(input_string)  
+
+# print("Number of vowels:", vowels)
+# print("Number of consonants:", consonants)
+
+
+# def get_file_extension(filename):
+#     # Split the filename by '.' to get the parts before and after the last '.'
+#     parts = filename.split('.')
+    
+#     # If there is more than one part (i.e., there is an extension), return the last part
+#     if len(parts) > 1:
+#         return parts[-1]
+#     else:
+#         return "No extension found"
+
+# # Test the function
+# input_filename = input("Enter a filename: ")
+# extension = get_file_extension(input_filename)
+# print("Extension of the file:", extension)

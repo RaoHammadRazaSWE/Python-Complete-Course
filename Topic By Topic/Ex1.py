c722516f409051acd75a3a0174033b2115a83152
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-07 01:49:34 +0900

    Ex1.py

---
+++ Topic By Topic/Ex1.py
@@ -0,0 +1,19 @@
+# Sum of Two numbers:
+
+First_num = input("Enter first number:")
+
+second_num = input("Enter second number:")
+
+sum = int(First_num) + int(second_num)
+
+print(sum)
+
+# Sum of Two Digit numbers:
+
+num = input("Enter a two digit numbers:")
+
+First_digit = num[0]
+
+second_digit = num[1]
+
+print(int(First_digit) + int(second_digit))

6c5ead26ab9ee0b7a1c048c2dbf186c902be5e32
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-09 04:48:57 +0900

    Line.py

---
+++ Topic By Topic/Line.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Rao Hammad Raza
+#
+# Created:     12/03/2024
+# Copyright:   (c) Rao Hammad Raza 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+for int in "234859":
+    print(int)

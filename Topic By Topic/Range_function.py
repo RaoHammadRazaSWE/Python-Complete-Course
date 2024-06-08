d42df475b99f04266f7b894b2a1890f23b1771ea
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-06-08 13:39:30 +0900

    Range_function.py

---
+++ Topic By Topic/Range_function.py
@@ -0,0 +1,10 @@
+# The range() function: In most cases, we will want to loop through a predetermined list.
+# However, we can also generate a list to loop over "on the fly" with the range() function.
+
+# for i in range(8):
+#     print(i)
+    
+# Printing Numbers
+# for num in range (1,6):
+#     print(num)    
+
